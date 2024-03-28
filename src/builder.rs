5f4a25080932ef945076de3d45d07ea798529d0b
Nathan Regner <nathanregner@gmail.com>
2024-03-27 20:41:49 -0600

    wip

--- src/builder.rs
+++ src/builder.rs
@@ -1,7 +1,51 @@
-use serde::{Deserialize, Serialize};
+use serde::{de::Visitor, Deserialize, Deserializer};
 use std::{collections::HashSet, fmt};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Hash, Eq, PartialEq, Clone, Copy, Debug)]
+pub struct MacAddress([u8; 6]);
+
+impl AsRef<[u8; 6]> for MacAddress {
+    fn as_ref(&self) -> &[u8; 6] {
+        &self.0
+    }
+}
+
+impl<'de> Deserialize<'de> for MacAddress {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct V;
+
+        impl<'de> Visitor<'de> for V {
+            type Value = MacAddress;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("a MAC address in the format 00:11:22:33:44:55")
+            }
+
+            fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                let mut bytes = [0u8; 6];
+                let mut iter = value.split(':');
+                for i in 0..6 {
+                    bytes[i] = u8::from_str_radix(
+                        iter.next().ok_or_else(|| E::custom("not enough bytes"))?,
+                        16,
+                    )
+                    .map_err(E::custom)?;
+                }
+                Ok(MacAddress(bytes))
+            }
+        }
+
+        deserializer.deserialize_str(V)
+    }
+}
+
+#[derive(Deserialize)]
 pub struct Builder {
     pub ssh_user: Option<String>,
     pub host_name: String,
@@ -11,7 +55,8 @@ pub struct Builder {
     pub mandatory_features: HashSet<String>,
     pub max_jobs: Option<u32>,
     pub speed_factor: Option<u32>,
-    pub mac_address: Option<u32>,
+    /// Optional MAC address to trigger wake-on-lan
+    pub mac_address: Option<MacAddress>,
 }
 
 impl fmt::Display for Builder {
--- src/hydra/state.rs
+++ src/hydra/state.rs
@@ -1,15 +1,22 @@
-use crate::builder::Builder;
+use crate::builder::{Builder, MacAddress};
 use std::{
     collections::{HashMap, HashSet},
-    sync::Mutex,
+    net::Ipv4Addr,
+    sync::{Arc, Mutex},
     time::{Duration, Instant},
 };
-use tokio::sync::watch::{channel, Receiver, Sender};
+use tokio::{
+    io,
+    net::UdpSocket,
+    sync::watch::{channel, error::RecvError, Receiver, Sender},
+};
+
+use super::client::HydraClient;
 
 pub struct BuilderState {
     builders: HashMap<String, Builder>,
     last_seen: Mutex<HashMap<String, Instant>>,
-    queued: Mutex<HashSet<String>>,
+    queued_systems: Mutex<HashSet<String>>,
     stale_after: Duration,
     changed: Sender<()>,
 }
@@ -22,7 +29,7 @@ impl BuilderState {
                 .into_iter()
                 .map(|b| (b.host_name.clone(), b))
                 .collect(),
-            queued: Mutex::new(HashSet::new()),
+            queued_systems: Mutex::new(HashSet::new()),
             last_seen: Mutex::new(HashMap::new()),
             stale_after,
             changed,
@@ -35,7 +42,7 @@ impl BuilderState {
 
     pub fn connect(&self, host_name: &str, instant: Instant) {
         if !self.builders.contains_key(host_name) {
-            tracing::warn!("Ignoring activation of unknown builder: {}", host_name);
+            tracing::warn!(?host_name, "Ignoring unknown host");
             return;
         }
 
@@ -50,8 +57,10 @@ impl BuilderState {
         drop(last_seen);
 
         if changed {
-            tracing::info!("Activated builder: {}", host_name);
+            tracing::debug!(?host_name, "Builder connected");
             let _ = self.changed.send(());
+        } else {
+            tracing::debug!(?host_name, "Received heartbeat from builder");
         }
     }
 
@@ -62,7 +71,7 @@ impl BuilderState {
         drop(last_seen);
 
         if changed {
-            tracing::info!("Deactivated builder: {}", host_name);
+            tracing::debug!(?host_name, "disconnected");
             let _ = self.changed.send(());
         }
     }
@@ -77,7 +86,7 @@ impl BuilderState {
                 if *at < stale {
                     builders.push(builder);
                 } else {
-                    tracing::warn!("Expiring stale builder: {}", host_name);
+                    tracing::debug!(?host_name, "Removed stale builder");
                     last_seen.remove(host_name);
                 }
             }
@@ -86,11 +95,115 @@ impl BuilderState {
         builders
     }
 
+    // TODO: cleanup
+    pub fn expire_stale(&self) {
+        self.get_connected();
+    }
+
     pub fn update_queued(&self, queued: impl IntoIterator<Item = String>) {
-        let mut queued = self.queued.lock().unwrap();
-        queued.clear();
-        queued.extend(queued);
-        *queued = queued.into_iter().collect();
+        let updated = queued.into_iter().collect();
+        let mut current = self.queued_systems.lock().unwrap();
+        if *current != updated {
+            *current = updated;
+            tracing::info!("Queue updated: systems = {:?}", *current);
+            let _ = self.changed.send(());
+        }
+    }
+
+    pub fn machines_to_wake(&self) -> Vec<MacAddress> {
+        let queued_systems = self.queued_systems.lock().unwrap().clone();
+        let connected = self
+            .get_connected()
+            .iter()
+            .map(|b| b.host_name.as_str())
+            .collect::<HashSet<_>>();
+
+        self.builders
+            .values()
+            .filter_map(|builder| {
+                let mac_address = builder.mac_address?;
+                if !connected.contains(&*builder.host_name)
+                    && queued_systems.contains(&builder.system)
+                {
+                    Some(mac_address)
+                } else {
+                    None
+                }
+            })
+            .collect()
+    }
+}
+
+pub enum Never {}
+
+#[tracing::instrument(skip_all)]
+pub async fn wake_builders(state: Arc<BuilderState>) -> Result<Never, RecvError> {
+    let mut sub = state.subscribe();
+    loop {
+        tokio::select! {
+            r = sub.changed() => r?,
+            _ = tokio::time::sleep(Duration::from_secs(30)) => {},
+        }
+
+        let mac_addresses = state.machines_to_wake();
+        if mac_addresses.is_empty() {
+            continue;
+        }
+        tracing::debug!("Broadcasting WOL packets to {:?}", mac_addresses);
+        if let Err(err) = wake_all(&mac_addresses).await {
+            tracing::error!(?err, "Failed to broadcast WOL packets");
+        };
+    }
+}
+
+pub async fn wake_all(mac_addresses: &[MacAddress]) -> io::Result<()> {
+    let to_addr = (Ipv4Addr::new(255, 255, 255, 255), 9);
+    let from_addr = (Ipv4Addr::new(0, 0, 0, 0), 0);
+    let socket = UdpSocket::bind(from_addr).await?;
+    socket.set_broadcast(true)?;
+
+    // TODO: parallel?
+    for mac_address in mac_addresses {
+        let packet = wake_on_lan::MagicPacket::new(mac_address.as_ref());
+        socket.send_to(packet.magic_bytes(), to_addr).await?;
+    }
+
+    Ok(())
+}
+
+pub async fn watch_queue(
+    state: Arc<BuilderState>,
+    client: HydraClient,
+) -> Result<Never, RecvError> {
+    let mut sub = state.subscribe();
+    loop {
+        tokio::select! {
+            r = sub.changed() => r?,
+            _ = tokio::time::sleep(Duration::from_secs(15)) => {},
+        }
+
+        let builds = match client.get_queue().await {
+            Ok(builds) => builds,
+            Err(err) => {
+                tracing::warn!(?err, "Failed to poll queue");
+                continue;
+            }
+        };
+
+        // TODO: log
+        state.update_queued(builds.into_iter().map(|b| b.system));
+    }
+}
+
+pub async fn keep_builders_awake(state: Arc<BuilderState>) -> Result<Never, RecvError> {
+    let mut sub = state.subscribe();
+    loop {
+        tokio::select! {
+            r = sub.changed() => r?,
+            _ = tokio::time::sleep(Duration::from_secs(30)) => {},
+        }
+
+        todo!()
     }
 }
 
@@ -110,6 +223,7 @@ mod tests {
                 mandatory_features: Default::default(),
                 max_jobs: None,
                 speed_factor: None,
+                mac_address: None,
             }],
         );
 
